8dea20820643bf4c1b3fdde2e1ae576599f41992
Adekoreday <khord4eng@gmail.com>
2020-05-13 05:38:46 +0100

    update home

---
+++ src/app/models/Event.ts
@@ -0,0 +1,12 @@
+export interface Event{
+    id: number;
+    name: string;
+    category: string;
+    isPremium: string;
+    location: string;
+    imageUrl: string;
+    date: string;
+    deadline: string;
+    createdAt: string;
+    updatedAt: string
+}
--- src/app/models/index.ts
+++ src/app/models/index.ts
@@ -1,4 +1,5 @@
 import  Login from './Login'
 import User from './User';
+import {Event} from './Event'
 
-export {Login, User}
+export {Login, User, Event}
